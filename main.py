c10213123d77c8043cc4bcad36b46782088b4b95
Bryan`Y. Limz <superuser@brylimz.com>
2021-12-03 14:03:14 +0700

    adding segments movement

--- main.py
+++ main.py
@@ -22,9 +22,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
 
+    for seg_num in range(len(segments)-1, 0, 1):
+        new_x = segments[seg_num-1].xcor()
+        new_y = segments[seg_num-1].xcor()
+        segments[seg_num].goto(new_x, new_y)
 
 
 
