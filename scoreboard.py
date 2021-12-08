772d542fbe84f65df3f2e6da8eb19e2fb6551a95
brylimz <superuser@brylimz.com>
2021-12-08 13:58:05 +0700

    i am committed

--- scoreboard.py
+++ scoreboard.py
@@ -1,8 +1,12 @@
 from turtle import Turtle
 
 
-class  Scoreboard(Turtle):
+class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.color("white")
+        self.penup()
         self.write(F"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.hideturtle()
+        self.goto(0, 270)
