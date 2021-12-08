c3b146c93e83aa697f5d6c08c657b632ce3f312b
brylimz <superuser@brylimz.com>
2021-12-08 13:50:56 +0700

    i am committed

--- scoreboard.py
+++ scoreboard.py
@@ -5,5 +5,4 @@ class  Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.write(F"Score: {self.score}", align="center", font=("Arial", 24, "normal")
-                   )
+        self.write(F"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
